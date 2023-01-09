5d28f085ef0611978978f4bc20aaf55ea7cdc75b
Thiago Junior Amorim De Melo (GOVBR GO - DRC Central) <thiago.melo@govbr.com.br>
2023-01-09 09:53:21 -0300

    Configurando components criados

--- src/app/endereco/endereco.module.ts
+++ src/app/endereco/endereco.module.ts
@@ -2,13 +2,18 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 
 import { EnderecoRoutingModule } from './endereco-routing.module';
+import { EnderecoFormComponent } from './endereco-form/endereco-form.component';
 
 
 @NgModule({
-  declarations: [],
+  declarations: [EnderecoFormComponent],
   imports: [
     CommonModule,
     EnderecoRoutingModule
+  ], exports: [
+    EnderecoFormComponent
   ]
+
+
 })
 export class EnderecoModule { }
