347798a678b5234e9979d75dae2f7bde83094272
Thiago Junior Amorim De Melo (GOVBR GO - DRC Central) <thiago.melo@govbr.com.br>
2023-01-09 09:44:45 -0300

    Criando o módulo de Address

---
+++ src/app/address/address.module.ts
@@ -0,0 +1,14 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+
+import { AddressRoutingModule } from './address-routing.module';
+
+
+@NgModule({
+  declarations: [],
+  imports: [
+    CommonModule,
+    AddressRoutingModule
+  ]
+})
+export class AddressModule { }
